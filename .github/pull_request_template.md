c87775c6f6036d06764aa07261a6b6ce120aad09
Henry Peters <meethenry09@gmail.com>
2026-03-03 10:41:21 +0100

    chore: add foundry-test

--- .github/pull_request_template.md
+++
@@ -1,9 +0,0 @@
-## Summary
-What does this PR implement?
-
-## Testing
-- [ ] Unit tests added
-- [ ] Tests passing locally
-
-## Notes
-Any assumptions or trade-offs?
--- assignments/Timelock/test/TimeLockV1.ts
+++
@@ -1,4 +0,0 @@
-import { expect } from "chai";
-import { Contract } from "ethers";
-import { network } from "hardhat";
-import { beforeEach, describe } from "node:test";
